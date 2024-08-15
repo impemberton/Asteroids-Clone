b46a4a818b8734c7b7713eca94e4de40151b6fe7
impemberton <isaac.mellad.pemberton@gmail.com>
2024-08-15 18:10:01 +0100

    Add rotation to meteors
    Meteors now rotate and sprite frame changes depending on rotation to simulate lighting

--- Scripts/meteor.gd
+++ Scripts/meteor.gd
@@ -1,16 +1,19 @@
 extends Area2D
 
 signal split(pos, health, repeat)
-
+var rotation_speed = randf_range(-2,2)
 var direction: Vector2 = Vector2.UP
 @export var speed: float = 50.0
 var health: int = 4
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 func _physics_process(delta: float) -> void:
+	sprite_2d.frame = wrapi((abs(int((rotation_degrees + 202.5) / 45))-4), 0, 8)
+
 	global_position = Vector2(wrapf(global_position.x, -10, 1162),wrapf(global_position.y, -10, 658))
 	position += direction * speed * delta
-
+	rotation_degrees += rotation_speed
 func take_damage():
 	health -= 1
 	if health > 0:
